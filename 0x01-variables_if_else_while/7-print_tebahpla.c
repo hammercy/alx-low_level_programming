233f5009a5f40f050b7d582726c0bcca75de03fd
Fesseha <fessehaenha@gmail.com>
2023-03-20 16:38:06 -0700

    Lowercase revers order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int az = 122;
+
+	while (az > 57)
+	{
+		putchar(az);
+		az--;
+	}
+	putchar('\n');
+	return (0);
+}
