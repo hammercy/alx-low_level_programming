8294a9592f6418d78fc05bf10ff3854118781aa9
Fesseha <fessehaenha@gmail.com>
2023-03-20 15:56:50 -0700

    Print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int az = 48;
+
+	while (az < 58)
+	{
+		putchar(az);
+		az++;
+	}
+	putchar('\n');
+	return (0);
+}
