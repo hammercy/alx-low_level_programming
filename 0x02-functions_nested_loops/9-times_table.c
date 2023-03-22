7e8ebed7a335f0fa3878a6760d1eb15215f38743
Fesseha <fessehaenha@gmail.com>
2023-03-21 20:11:06 -0700

    Table of 1-9 mutiplication fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * times_table - Prints table the contains multiple of 1-9
- *
  */
 void times_table(void)
 {
@@ -17,35 +15,36 @@ void times_table(void)
 			x = i * j;
 			xy = 48 + x % 10;
 			xx = 48 + x / 10;
-			xc = 48 + x;
-			if (j < 9  && x < 10)
+			if (x < 10)
 			{
 				if (j != 0)
 					_putchar(' ');
-				_putchar(xc);
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if (j < 9 && x >= 10)
-			{
-				_putchar(xx);
 				_putchar(xy);
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if (j == 9 && x < 10)
-			{
-				_putchar(' ');
-				_putchar(xc);
-				_putchar('\n');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('\n');
+				}
 			}
 			else
 			{
 				_putchar(xx);
 				_putchar(xy);
-				_putchar('\n');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('\n');
+				}
 			}
-		}
 
+		}
 	}
 }
