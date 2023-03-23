9daabff2a4e71d150c1cf45b62dc3be3f8b7e7e0
Fesseha <fessehaenha@gmail.com>
2023-03-23 13:17:15 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers(void)
+ *
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
