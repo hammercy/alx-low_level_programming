045b37b6ffd9573a7c9ede9d5b8960c7fd3aa192
Fesseha <fessehaenha@gmail.com>
2023-03-23 14:29:43 -0700

    print  1-14 10times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers- print numbers 0-14
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+				_putchar(49);
+			_putchar((i % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
