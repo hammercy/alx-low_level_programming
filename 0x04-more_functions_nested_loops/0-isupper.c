e7a4eece4b60a5d5376c797608039c027534b6be
Fesseha <fessehaenha@gmail.com>
2023-03-23 11:22:43 -0700

    is upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _isupper - Checks wether a character is uppercase or not
+ * @c : a character
+ *
+ *Return: 1-upppercase  0-lowercase
+ */
+int _isupper(int c)
+{
+	if (c  > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
