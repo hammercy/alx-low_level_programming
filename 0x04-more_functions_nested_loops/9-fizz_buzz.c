e03a72f7d166e549d004720e1afe2efb406ebf92
Fesseha <fessehaenha@gmail.com>
2023-03-23 18:43:45 -0700

    Fizz buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,6 @@
  * Description: This functions prints number between 1-100
  * followed by new line. But for multiple of three , and
  * five it prints fizz and buzz respectively. For multiply
- * both it prints fizzbuzz.
  * Return: 0 -success
  */
 int main(void)
@@ -17,13 +16,24 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz ");
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
 		else if ((i % 3 == 0) && (i % 5 != 0))
-			printf("Fizz ");
+		{
+			printf("Fizz");
+			printf(" ");
+		}
 		else if ((i % 3 != 0) &&  (i % 5 == 0))
-			printf("Buzz ");
+		{
+			printf("Buzz");
+			printf(" ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
 	printf("\n");
 	return (0);
