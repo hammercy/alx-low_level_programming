626025227f5217eba2c3774b05a774f7ccfff598
Fesseha <fessehaenha@gmail.com>
2023-03-27 16:06:58 -0700

    Pointers,arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -19,3 +19,24 @@ void print_rev(char *s)
 		_putchar(c);
 	}
 }
+/**
+ * _strlen - counts the character in string array
+ * @s: pointer to a string
+ *
+ * Return: strlen- the length of the string array
+ */
+int _strlen(char *s)
+{
+	int i;
+	int strlen;
+
+	i = 0;
+	strlen = 0;
+	while (*(s + i) != '\0')
+	{
+		strlen = strlen + 1;
+		i++;
+	}
+	return (strlen);
+}
+
