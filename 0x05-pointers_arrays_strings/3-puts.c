2c688ee1f9280964eb18b0d4473fc4bc03321e6e
Fesseha <fessehaenha@gmail.com>
2023-03-27 16:00:09 -0700

    Pointers,arrays and strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,9 +10,15 @@
 
 void _puts(char *str)
 {
-	int blen;
+	int blen, i;
 
-	blen = _strlen(str);
+	blen = 0;
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		blen = blen + 1;
+		i++;
+	}
 	write(1, str, blen);
 	_putchar('\n');
 }
