9e28fd63d1d3765f78ea1611f5d4b2c7a6a1ca83
Fesseha <fessehaenha@gmail.com>
2023-03-27 17:32:44 -0700

    Pointers,arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,15 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
-	while (*src != '\0')
+	if (dest != Null && src != Null)
 	{
-		*(dest + i) = *(src + i);
-		i++;
+		while (*src != '\0')
+		{
+			*(dest + i) = *(src + i);
+			i++;
+		}
+		*(dest + i) = '\0';
 	}
-	*(dest + i) = '\0';
 
 	return (dest);
 }
