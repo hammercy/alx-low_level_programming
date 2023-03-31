a7130cdd7086f604a7849272037d97f234a42e39
Fesseha <fessehaenha@gmail.com>
2023-03-31 06:22:51 -0700

    concatenate string at the end

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,19 +13,56 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 	int m;
+	int n;
 
 	j = 0;
-	m = sizeof(src);
-	i = 0;
-	while (dest[i] != '\0')
+	m = _strlen(src);
+	n = _strlen(dest);
+	i = n;
+	while( j < m)
 	{
+		*(dest + i) = *(src + j);
+		j++;
 		i++;
 	}
-	for (i = i; j < m; i++)
-	{
-		dest[i] = src[j];
-		j++;
-	}
-	dest[i] = '\0';
+
 	return (dest);
+}
+
+/**
+ * _strlen - counts the character in string array
+ * @s: pointer to a string
+ *
+ * Return: strlen- the length of the string array
+ */
+int _strlen(char *s)
+{
+	int strlen;
+
+	strlen = 0;
+	while (*(s + strlen) != '\0')
+	{
+		strlen++;
+	}
+
+	return (strlen);
+
+}
+/**
+ * _cntnull - countes the null character in string
+ * @str: pointer to the string
+ * @n:  index for the start of null character
+ *
+ * Return: nl - number of nul characters
+ */
+int _cntnull(char *str, int n)
+{
+	int nl ;
+
+	nl = n;
+	while( *(str + nl) == '\0')
+	{
+		nl++;
+	}
+	return (nl);
 }
