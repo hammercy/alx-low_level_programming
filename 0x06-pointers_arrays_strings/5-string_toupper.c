6a4a623b9d88d0a66a397b4d23f931aa99d9b158
Fesseha <fessehaenha@gmail.com>
2023-03-31 08:14:02 -0700

    change to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,25 +5,40 @@
  * string_toupper - changes the lowercase char to uppercase in string
  * @srt: pinter to string
  *
- * Return: str pointer to string
+ * Return: str-pointer to string
  */
 
 char *string_toupper(char *str)
 {
 	int m, i;
 
-	m = sizeof(str);
-	for (i = 0; i < m; i++)
+	m = _strlen(str);
+	i = 0;
+	while (i < m && str[i] != '\0')
 	{
-		if (str[i] == '\0')
-		{
-			break;
-		}
-		else
-		{
-			if (str[i] > 96 && str[i] < 123)
-				str[i] = str[i] - 32;
-		}
+		if (str[i] > 96 && str[i] < 123)
+			str[i] = str[i] - 32;
+		i++;
 	}
 	return (str);
+}
+
+/**
+ * _strlen - counts the character in string array
+ * @s: pointer to a string
+ *
+ * Return: strlen- the length of the string array
+ */
+int _strlen(char *s)
+{
+	int strlen;
+
+	strlen = 0;
+	while (*(s + strlen) != '\0')
+	{
+		strlen++;
+	}
+
+	return (strlen);
+
 }
