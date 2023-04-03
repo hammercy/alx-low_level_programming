f38524c499160db2b77f06f605371ee0eec06119
Fesseha <fessehaenha@gmail.com>
2023-04-03 14:27:45 -0700

    0-memoset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - assing byte b to each elements of thae array
+ * @s: pointer to array of char
+ * @b: char data type
+ * @n:  size of array
+ *
+ *Return: returns pointer to the char array;
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+
+}
