2686c4918c3aeb0ac5cbdf8595b9e03b7a5528de
Fesseha <fessehaenha@gmail.com>
2023-04-10 18:51:54 -0700

    str concatenate malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+/**
+ * str_concat - concatinates two strings
+ * @s1: 1st str over which- 2nd str appended
+ * @s2: 2nd str to be copied
+ *
+ * Return: 1st str
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int lens1, lens2, i, lenptr;
+	char *strptr;
 
+	lens1 = lens2 = 0;
+	if (s1 == NULL && s2 == NULL)
+	{
+
+	}
+	else if (s1 == NULL)
+	{
+		lens2 = _strlen(s2);
+	}
+	else if (s2 == NULL)
+	{
+		lens1 = _strlen(s1);
+	}
+	else
+	{
+		lens1 = _strlen(s1);
+		lens2 = _strlen(s2);
+	}
+	lenptr = lens1 + lens2;
+	strptr = (char *) malloc((lenptr + 1) * sizeof(char));
+	if (strptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < lenptr; i++)
+		{
+			if (i < lens1)
+				strptr[i] = s1[i];
+			else
+				strptr[i] = s2[i - lens1];
+		}
+		return (strptr);
+	}
+}
 /**
  * _strlen - counts the character in string array
  * @s: pointer to a string
@@ -21,57 +68,4 @@ int _strlen(char *s)
 		i++;
 	}
 	return (strlen);
-}
-
-/**
- * str_concat - concatinates two strings
- * @s1: 1st str over which- 2nd str appended
- * @s2: 2nd str to be copied
- *
- * Return: 1st str
- */
-
-char *str_concat(char *s1, char *s2)
-{
-	unsigned int lens1, lens2, i, lenptr;
-	char *strptr;
-
-	if (s1 == NULL && s2 == NULL)
-	{
-		lens1 = 0;
-		lens2 = 0;
-	}
-	else if (s1 == NULL)
-	{
-		lens1 = 0;
-		lens2 = _strlen(s2);
-	}
-	else if (s2 == NULL)
-	{
-		lens2 = 0;
-		lens1 = _strlen(s1);
-	}
-	else
-	{
-		lens1 = _strlen(s1);
-		lens2 = _strlen(s2);
-	}
-	lenptr = lens1 + lens2;
-	strptr = (char *) malloc((lenptr + 1) * sizeof(char));
-
-	if (strptr == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		for (i = 0; i < lenptr; i++)
-		{
-			if (i < lens1)
-				strptr[i] = s1[i];
-			else
-				strptr[i] = s2[i - lens1];
-		}
-		return (strptr);
-	}
 }
