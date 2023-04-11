88bb8cf5aadf2a84d850c1b8d6b4957bfc607b1e
Fesseha <fessehaenha@gmail.com>
2023-04-11 06:34:31 -0700

    grid allocate error fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,18 +12,23 @@
 int **alloc_grid(int width, int height)
 {
 	int i, j, **arr2d;
-
+	arr2d = NULL;
 	if (width < 1 || height < 1)
-		return (NULL);
+		return (arr2d);
 
 	arr2d = (int **) malloc(height * sizeof(int *));
 	if (arr2d == NULL)
-		return (NULL);
+		return (arr2d);
 	for (i = 0; i < width; i++)
 	{
 		arr2d[i] = (int *) malloc(width * sizeof(int));
 		if (arr2d[i] == NULL)
+		{
+			for (j = i; j > -1; j--)
+				free(arr2d[i]);
+			free(arr2d);
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
