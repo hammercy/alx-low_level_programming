0629c0dc8f5a8658b0e17e2997978fcab655671a
Fesseha <fessehaenha@gmail.com>
2023-04-11 15:50:59 -0700

    Split into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,13 +13,9 @@ char **strtow(char *str)
 	unsigned int row = 0, col = 0;
 	char **wrdptr;
 
-	if (str == NULL || str[0] == '\0' || str[0] == '\b' || str[0] == ' ')
+	row = _cntwrds(str);
+	if (row == 0)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ')
-			row++;
-	}
 	wrdptr = (char **) malloc((row + 2) * sizeof(char *));
 	if (wrdptr == NULL)
 		return (NULL);
@@ -40,4 +36,49 @@ char **strtow(char *str)
 	}
 	wrdptr[j + 1] = NULL;
 	return (wrdptr);
+}
+/**
+ * _cntwrds - counts words in a string
+ * @str: string to be counted
+ *
+ * Return: number of words
+ */
+int _cntwrds(char *str)
+{
+	int cnt, i;
+
+	cnt = 0;
+	if (str == NULL)
+		return (0);
+	else if (_strlen(str) == 0)
+		return (0);
+	else if (_strlen(str) == 1 && (str[0] == ' ' || str[0] == '\b'))
+		return (0);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			cnt++
+	}
+	cnt++;
+	return (cnt);
+}
+/**
+ * _strlen - counts the character in string array
+ * @s: pointer to a string
+ *
+ * Return: strlen- the length of the string array
+ */
+int _strlen(char *s)
+{
+	int i;
+	int strlen;
+
+	i = 0;
+	strlen = 0;
+	while (*(s + i) != '\0')
+	{
+		strlen = strlen + 1;
+		i++;
+	}
+	return (strlen);
 }
