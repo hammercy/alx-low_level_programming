5c1fc5c9d9224d2d9b4521d6c2cc140b224ee5ac
Fesseha <fessehaenha@gmail.com>
2023-04-10 18:26:05 -0700

    malloc str concatnate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -43,9 +43,18 @@ char *str_concat(char *s1, char *s2)
 	else
 	{
 		if (s1 == NULL)
+		{
 			lens1 = 0;
+		}
 		else if (s2 == NULL)
+		{
 			lens2 = 0;
+		}
+		else
+		{
+			lens1 = _strlen(s1);
+			lens2 = _strlen(s2);
+		}
 	}
 	lenptr = lens1 + lens2;
 	strptr = (char *) malloc((lenptr + 2) * sizeof(char));
