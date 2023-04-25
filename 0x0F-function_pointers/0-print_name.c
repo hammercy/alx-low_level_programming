2bcb5b410869851cb2791bb034b7ce88214778c1
Fesseha <fessehaenha@gmail.com>
2023-04-25 13:53:11 -0700

    pointer to function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - this function prints a name
+ * @name: the name to be printed.
+ * @f: function that prints the name.
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+        f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+This is pointer to a functions and how to declare ,
+intialize and call it. What its special advantage is.
