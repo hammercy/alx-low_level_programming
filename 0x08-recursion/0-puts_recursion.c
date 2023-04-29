c73f7d03e1f79d1467ac1cfe209a671f533f7417
Fesseha <fessehaenha@gmail.com>
2023-04-29 06:49:23 -0700

    puts using recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 
 /**
  * _puts_recursion - prints a string in recursive way
@@ -10,11 +10,13 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] == '\0')
+	if (s == NULL || s[i] == '\0')
+	{
+		_putchar('\n');
 		return;
-
+	}
 	_putchar(s[i]);
-       	i++;
+	i = i + 1;
 	if (s[i] != '\0')
 		_puts_recursion(&s[i]);
 	else
