368088e833d1d01d1ff329d4c232b69ac010f124
Fesseha <fessehaenha@gmail.com>
2023-05-03 16:15:10 -0700

    Print safely

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,45 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ * find_loop - finds the loop in a linked list
+ * @head: pointer to the linked list
+ *
+ * Return: address of the start of loop , NULL otherwise
+ */
+const listint_t *find_loop(const listint_t *head)
+{
+	const listint_t *ptrslow = head, *ptrfast = head, *ptrcrnt = head;
+	int i, lopcnt = 0, time = 4;
+
+	while (ptrslow && ptrfast && ptrfast->next)
+	{
+		ptrslow = ptrslow->next;
+		ptrfast = ptrfast->next->next;
+		if (ptrslow == ptrfast)
+			time++;
+		if (time == 5 && (ptrfast != ptrslow))
+			lopcnt++;
+		if (time == 6)
+		{
+			while (time && ptrcrnt && ptrslow)
+			{
+				for(i = 0; i < lopcnt; i++)
+				{
+					if (ptrcrnt == ptrslow)
+						break;
+					ptrslow = ptrslow->next;
+				}
+				if (ptrcrnt != ptrslow)
+					ptrcrnt = ptrcrnt->next;
+				else
+					return (ptrcrnt);
+			}
+		}
+	}
+	return (NULL);
+}
+
+/**
  * print_listint_sage - this fucntion prints listint_t linked list safely.
  * @head: pointer to pointer of the linked list to be reversed.
  *
@@ -9,15 +48,35 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t cnt;
-	const listint_t *ptrcrnt;
 
-	ptrcrnt = head;
+	const listint_t *ptrc, *ptrs;
+	size_t cnt = 0;
+	unsigned int time;
 
-	for (cnt = 0; ptrcrnt != NULL; ++cnt)
+	if (head == NULL)
 	{
-		printf("[%p] %d\n", (void *)ptrcrnt, ptrcrnt->n);
-		ptrcrnt = ptrcrnt->next;
+		printf("-> [%p] %d", (void *)head, 98);
+		exit(98);
+	}
+	ptrs = head;
+	ptrc = find_loop(head);
+	if (ptrc == NULL)
+		time = 4;
+	else
+		time = 0;
+	while (time < 1 && ptrs)
+	{
+		printf("[%p] %d\n", (void *)ptrs, ptrs->n);
+		ptrs = ptrs->next;
+		cnt++;
+		if (ptrs == ptrc)
+			time++;
+	}
+	while (head && time == 4)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		cnt++;
 	}
 	return (cnt);
 }
