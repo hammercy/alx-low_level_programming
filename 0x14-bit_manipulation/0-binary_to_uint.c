cce8aa90dec750612900a67a58ceafddfa5dd650
Fesseha <fessehaenha@gmail.com>
2023-07-06 04:25:25 -0700

    binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
  * binary_to_uint - convert array of string of 0 and 1 to decimal.
  * @b: pointer to zero and ones
  *
- * Return: the number on success otherwise 0 if there char that is not 1 0r 0 or b = NULL;
+ * Return: the number|0-b isnot 1 0r 0 or b = NULL;
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -14,20 +14,21 @@ unsigned int binary_to_uint(const char *b)
 	uint inum = 0;
 	uint strln = 0;
 
+	if (b == NULL)
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		strln++;
 		mult2 = mult2 * 2;
 	}
 	i = 0;
-	while(b[i] > 47 && b[i] < 50 && i < strln)
+	while (i < strln)
 	{
+		if (b[i] < 48 && b[i] > 49)
+			return (0);
 		inum = inum + (mult2 * ((int)b[i] - 48));
-		mult2 = mult2/2;
+		mult2 = mult2 / 2;
 		i++;
 	}
 	return (inum);
 }
-		
-		
-		
