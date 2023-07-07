918767f64e47b6c855bd37392219829df6f142d5
Fesseha <fessehaenha@gmail.com>
2023-07-06 19:13:28 -0700

    set bit at index n

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * set_bit - set a bit at given index to 1
+ * @n: holdes the given number
+ * @index: holds the index value of the bit
+ *
+ * Return: index of the bit or -1 if an error occured.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int msk01, msk02, msk10, msk;
+	unsigned int i;
+
+	msk01 = 1;
+	msk10 = 1;
+	msk02 = 2;
+	while (msk02 != 0)
+	{
+		msk02 <<= 1;
+		msk10 <<= 1;
+	}
+
+	for (i = 0; i < index; i++)
+		msk01 <<= 1;
+	if (msk01 == 0)
+		return (-1);
+	*n = *n | msk01;
+	msk = *n & msk01;
+	if (msk != 0)
+		return (1);
+	else
+		return (-1);
+}
