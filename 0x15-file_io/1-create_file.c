6267fe31c261e2c82c5eca12df3c40014b3d7a47
Fesseha <fessehaenha@gmail.com>
2023-07-13 04:19:23 -0700

    Create file fix error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, flgc, mode);
 	if (fd == fail)
 	{
-		if (errno == EACCES)
+		if (errno == EEXIST)
 		{
 			fd = open(filename, flgo);
 			if (fd == fail)
