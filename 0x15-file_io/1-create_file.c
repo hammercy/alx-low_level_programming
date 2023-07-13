5c9482d30eb3659cadef9632b253b91431507fc4
Fesseha <fessehaenha@gmail.com>
2023-07-13 03:54:20 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,6 +7,7 @@
  *
  * DESCRIPTION: The created file must have those permissions: rw-------.
  *              if the file exist, the permission is not changed.
+ *              if the file aready exist, truncate it.
  *              if the file name is NULL returns -1.
  *              if the text_content is NULL create an empty file.
  * Return: 1-success: -1-failure.
@@ -15,29 +16,43 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int flag;
+	int flgc, flgo;
 	mode_t mode;
-	int wcnt = 0, i = 0;
+	size_t wcnt = 0, i = 0;
+	ssize_t swrt = 0;
+	int fail = -1, succ = 1;
 	char *strempty = "";
 
+	mode = S_IRUSR | S_IWUSR;
+	flgc = O_RDWR | O_CREAT | O_EXCL;
+	flgo = O_RDWR | O_TRUNC
 	if (filename == NULL)
-		return (-1);
+		return (fail);
 	if (text_content == NULL)
 		text_content = strempty;
-	mode = S_IRUSR | S_IWUSR;
-	flag = O_WRONLY | O_CREAT | O_TRUNC;
-	fd = open(filename, flag, mode);
-	if (fd == -1)
-		return (-1);
-	while (text_content[i] != '\0')
+	fd = open(filename, flgc, mode);
+	if (fd == fail)
 	{
-		wcnt += write(fd, text_content, 1);
-		i++;
+		if (errno == EACCES)
+		{
+			fd = open(filename, flago);
+			if (fd == fail)
+				return (fail);
+		}
+		else
+		{
+			return (fail);
+		}
 	}
-	if (wcnt == -1)
-		return (-1);
+	for (i = 0; text_content[i] != '\0'; i++)
+		wcnt++;
+
+	if (wcnt != 0)
+		swrt = write(fd, text_content, wcnt);
+	if (swrt == -1)
+		return (fail);
 	fsync(fd);
-	if (close(fd) == -1)
-		return (-1);
-	return (1);
+	if (close(fd) == fail)
+		return (fail);
+	return (succ);
 }
