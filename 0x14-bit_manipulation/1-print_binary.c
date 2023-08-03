d515d98e1dab49bf029ffbbfc5d80a2e41666d10
Fesseha Endrias <fessehaenha@gmail.com>
2023-08-03 08:50:06 -0700

    Integer to binary and print fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,71 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_binary - print binary represenation of a number
  * @n: the number to be represented in binary
+ *
+ * Description: You are not allowed to use arrays
+ *		You are not allowed to use malloc
+ *		You are not allowed to use the % or / operators.
  *
  */
 
 void print_binary(unsigned long int n)
 {
-	char *iptr, *rptr;
-	int i;
+	unsigned long int msk01, msk02;
+	unsigned long int msk, msk001;
+	int strt = 0;
 
-	iptr = NULL;
-	rptr = _dtob(n, &iptr);
-	if (rptr == NULL)
-	{
-		;
-	}
-	else
-	{
-		for (i = 0; rptr[i] != '\0'; i++)
-			_putchar(rptr[i]);
-	}
-	if (iptr != NULL)
-		free(iptr);
-}
-/**
- * _dtob - change an unsigned long int to binary sequenc of characters.
- * @inum: an integer to be converted to binary.
- * @iptr: pointer to z whole malloc allocated memory to be used for freeing
- *
- * Return: pointer to the start of the binary number.
- */
+	msk01 = 1;
+	msk02 = 2;
+	msk001 = 1;
 
-char *_dtob(ulint inum, char **iptr)
-{
-	int max_dGt = 65, i;
-	char *rptr;
-	ulint mask = 1, mskd;
-
-	*iptr = (char *)malloc((max_dGt + 1) * sizeof(char));
-	if (iptr[0] == NULL)
-		return (NULL);
-	(*iptr)[max_dGt] = '\0';
-	if (inum == 0)
+	while (msk02 > 0)
 	{
-		(*iptr)[max_dGt - 1] = '0';
-		rptr = &((*iptr)[max_dGt - 1]);
-		return (rptr);
+		msk01 <<= 1;
+		msk02 <<= 1;
 	}
-	for (i = max_dGt - 1; mask >= 1; i--)
+
+	if (n == 0)
+		_putchar('0');
+	else if (n > 0)
 	{
-		mskd = mask & inum;
-		if (mskd == mask)
+		while (msk001 > 0)
 		{
-			(*iptr)[i] = '1';
-			rptr = &((*iptr)[i]);
+			msk = n & msk01;
+			if (msk != 0)
+				strt++;
+			if (msk == 0 && strt > 0)
+				_putchar('0');
+			else if (msk != 0 && strt > 0)
+				_putchar('1');
+			n <<= 1;
+			msk001 <<= 1;
 		}
-		else if (mskd == 0)
-		{
-			(*iptr)[i] = '0';
-		}
-		mask = mask << 1;
-		mskd = 0;
 	}
-/*	for (i = 0; rptr[i] != '\0'; i++)
-		printf("%c", rptr[i]);
-		printf("\n");*/
-	return (rptr); 
 }
