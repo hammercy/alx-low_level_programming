0e67229a17b2934364a260eb0b52cea558f0ce53
Fesseha Endrias <fessehaenha@gmail.com>
2023-08-03 06:06:14 -0700

    Binary to integer fix error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,22 +7,19 @@
  * Return: the number|0-b isnot 1 0r 0 or b = NULL;
  */
 
-unsigned int binary_to_uint(const char *b)
+uint binary_to_uint(const char *b)
 {
 	uint mult2 = 1;
-	uint i;
+	int i;
 	uint inum = 0;
 	uint strln = 0;
 	uint m01;
 
 	if (b == NULL)
 		return (0);
-	for (i = 1; b[i] != '\0'; i++)
-	{
-		strln++;
-		mult2 = mult2 * 2;
-	}
 	for (i = 0; b[i] != '\0'; i++)
+		strln++;
+	for (i = strln - 1; i > -1; i--)
 	{
 		if (b[i] == '0')
 			m01 = 0;
@@ -31,7 +28,7 @@ unsigned int binary_to_uint(const char *b)
 		else
 			return (0);
 		inum = inum + (mult2 * m01);
-		mult2 = mult2 / 2;
+		mult2 = mult2 * 2;
 	}
 	return (inum);
 }
