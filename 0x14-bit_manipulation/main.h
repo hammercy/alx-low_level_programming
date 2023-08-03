fb9f58334c4114ba459b48d77e06bf0fa5c0b4c3
Fesseha Endrias <fessehaenha@gmail.com>
2023-08-03 08:05:50 -0700

    Integer to binary and print fixed

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 
 typedef unsigned int uint;
+typedef unsigned long int ulint;
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+char *_dtob(ulint inum, char *iptr);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
